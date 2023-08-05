659a1bdc7743c2eb609c3b9cb8762dd3f25600f7
Neaz Ahsan <neaz.ahsan@bracits.com>
2023-08-05 12:55:07 +0600

    init commit

---
+++ src/main/java/com/example/atc/data/Message.java
@@ -0,0 +1,10 @@
+package com.example.atc.data;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    private String senderName;
+    private String targetUserName;
+    private String message;
+}
